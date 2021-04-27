131627251065761817abeef56c7d045dc0957a8a
bbbootstrap <50336543+bbbootstrap@users.noreply.github.com>
2021-04-27 11:57:58 +0530

    Update jquery.toolbar.js

--- jquery.toolbar.js
+++ jquery.toolbar.js
@@ -1,3 +1,19 @@
+/**
+ * Toolbar.js
+ *
+ * @fileoverview  jQuery plugin that creates tooltip style toolbars.
+ * @link          http://paulkinzett.github.com/toolbar/
+ * @author        Paul Kinzett (http://kinzett.co.nz/)
+ * @version       1.1.0
+ * @requires      jQuery 1.7+
+ *
+ * @license jQuery Toolbar Plugin v1.1.0
+ * http://paulkinzett.github.com/toolbar/
+ * Copyright 2013 - 2015 Paul Kinzett (http://kinzett.co.nz/)
+ * Released under the MIT license.
+ * <https://raw.github.com/paulkinzett/toolbar/master/LICENSE.txt>
+ */
+
 if ( typeof Object.create !== 'function' ) {
     Object.create = function( obj ) {
         function F() {}
